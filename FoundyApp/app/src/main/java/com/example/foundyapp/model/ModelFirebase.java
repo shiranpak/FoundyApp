5c5c5c01744e5ee3861b0598f6ca6804296e7808
rptcubic <rptcubic@gmail.com>
2022-03-05 16:09:04 +0200

    firebase setup

---
+++ FoundyApp/app/src/main/java/com/example/foundyapp/model/ModelFirebase.java
@@ -0,0 +1,7 @@
+package com.example.foundyapp.model;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ModelFirebase {
+    //FirebaseFirestore db = FirebaseFirestore.getInstance();
+}
