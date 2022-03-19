4a7439c238f02583b05a400271bf390f5027ad92
shiran <shiranp1234@gmail.com>
2022-03-19 12:45:21 +0200

    add post entity

--- FoundyApp/app/src/main/java/com/example/foundyapp/model/Post.java
+++ FoundyApp/app/src/main/java/com/example/foundyapp/model/Post.java
@@ -1,12 +1,25 @@
 package com.example.foundyapp.model;
 
+import android.media.Image;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Post {
 
+    final public static String COLLECTION_NAME = "posts";
+
+    @PrimaryKey
+    @NonNull
     private int postId;
     private String category;
     private String location;
     private String date;
+    private Image image;
     private String description;
     private enum type {lost,found}
     private int userId;
+    private boolean isDeleted;
 }
